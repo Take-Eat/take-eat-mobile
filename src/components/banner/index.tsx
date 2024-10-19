e9ab6c437a7df2e984b5875708fd585b6f79472a
isabelle9912 <isabellesaahirah28@gmail.com>
2024-10-19 20:14:18 -0300

    feat: component banner

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,49 @@
+import { View, Pressable, Image } from "react-native";
+import PagerView from "react-native-pager-view";
+
+export function Banner() {
+  return (
+    <View className="w-full h-36 md:h-60 rounded-2xl mt-5 mb-4">
+      <PagerView style={{ flex: 1 }} initialPage={0} pageMargin={14}>
+        <Pressable
+          className="w-full h-36 md:h-60 rounded-2xl"
+          key="1"
+          onPress={() => {
+            console.log("CLICOU NO BANNER 2");
+          }}
+        >
+          <Image
+            source={require("../../assets/images/banner1.png")}
+            className="w-full h-36 md:h-60 rounded-2xl"
+          />
+        </Pressable>
+
+        <Pressable
+          className="w-full h-36 md:h-60 rounded-2xl"
+          key="1"
+          onPress={() => {
+            console.log("CLICOU NO BANNER 2");
+          }}
+        >
+          <Image
+            source={require("../../assets/images/banner2.png")}
+            className="w-full h-36 md:h-60 rounded-2xl"
+          />
+        </Pressable>
+
+        <Pressable
+          className="w-full h-36 md:h-60 rounded-2xl"
+          key="1"
+          onPress={() => {
+            console.log("CLICOU NO BANNER 3");
+          }}
+        >
+          <Image
+            source={require("../../assets/images/banner3.png")}
+            className="w-full h-36 md:h-60 rounded-2xl"
+          />
+        </Pressable>
+      </PagerView>
+    </View>
+  );
+}
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,12 +4,14 @@ import { Colors } from "../../constants/Colors";
 
 export function Header() {
   return (
-    <View className="w-full flex-row justify-between items-center">
+    <View className="w-full flex flex-row justify-between items-center">
       <Pressable>
-        <Image source={require("@/assets/images/logo_take_eat_black.png")} />
+        <Image source={require("../../assets/images/logo_take_eat_black.png")} />
       </Pressable>
       <Pressable>
         <Ionicons name="notifications" size={30} color="#F58F00" />
+        {/* <Feather name="bell" size={20} color="#F58F00" /> */}
+        {/* Olha  esse icone depois ^^^^^*/}
       </Pressable>
     </View>
   );
