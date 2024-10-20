7112f0b99cef560ea8b1187c58b9b0f59baefe68
isabelle9912 <isabellesaahirah28@gmail.com>
2024-10-19 21:00:24 -0300

    fiz: estilo do input

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -3,12 +3,17 @@ import { TextInput, View } from "react-native";
 
 export function Search() {
   return (
-    <View className="w-full md:h-60 flex flex-row justify-between items-center h-14 px-2">
-      <Feather name="filter" size={30} color="#F58F00" />
-      <View className="p-10 py-4 rounded-2xl bg-slate-100">
-        <TextInput placeholder="Procure alimentos..." />
+    <View className="w-full md:h-60 flex flex-row items-center h-14 px-2 gap-3">
+      <Feather name="filter" size={30} color="#F58F00" className="flex-none" />
+
+      <View className="flex-1 p-10 py-4 rounded-2xl bg-slate-100">
+        <TextInput
+          placeholder="Procure alimentos..."
+          className="w-full h-full flex-1"
+        />
       </View>
-      <Feather name="search" size={30} color="#F58F00" />
+
+      <Feather name="search" size={30} color="#F58F00" className="flex-none"/>
     </View>
   );
 }
