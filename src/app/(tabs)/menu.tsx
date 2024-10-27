785eb63df684a3a63d68ff0e4bbf90816a913ede
isabelle9912 <isabellesaahirah28@gmail.com>
2024-10-26 23:03:08 -0300

    feat: links na screen

--- src/app/(tabs)/menu.tsx
+++ src/app/(tabs)/menu.tsx
@@ -1,5 +1,5 @@
 import { Image, Pressable, Text, View } from "react-native";
-import { Card, TabLayout } from "@components";
+import { CardMenu, TabLayout } from "@components";
 import { useEffect, useState } from "react";
 import { Link } from "expo-router";
 import { AntDesign, Ionicons } from "@expo/vector-icons";
@@ -67,12 +67,31 @@ export default function Menu() {
               </Pressable>
             </View>
 
-            <Link
-              href={"/(menu)/termsOfUse"}
-              className="w-full h-24 bg-gray-700"
-            >
-              <View className="flex-1 flex justify-between">
-                <View className="flex-row">
+            <Link href={"/(menu)/notifications"} style={{ width: "100%" }}>
+              <CardMenu
+                color="bg-gray-700"
+                iconName="notifications"
+                iconSize={30}
+                title="Notificações"
+                titleColor="text-black"
+                titleSize="text-xl"
+              />
+            </Link>
+
+            <Link href={"/(menu)/address"} style={{ width: "100%" }}>
+              <CardMenu
+                color="bg-gray-700"
+                iconName="location-sharp"
+                iconSize={30}
+                title="Localização"
+                titleColor="text-black"
+                titleSize="text-xl"
+              />
+            </Link>
+
+            <Link href="/(menu)/termsOfUse" style={{ width: "100%" }}>
+              <View className="w-full h-24 bg-gray-700 flex-row justify-between items-center px-5">
+                <View className="flex-row gap-3 items-center">
                   <Ionicons name="newspaper" size={30} color={"#F58F00"} />
 
                   <Text className="text-lg font-semibold text-black">
@@ -82,6 +101,36 @@ export default function Menu() {
 
                 <AntDesign name="right" size={30} />
               </View>
+            </Link>
+
+            {/* <Link
+              href="/(menu)/termsOfUse"
+              style={{ width: "100%" }}
+              className="bg-blue-500 h-24 flex-row justify-between"
+            >
+              <View className="bg-red-600 h-24 flex-row items-center gap-3">
+                <Ionicons name="add" size={30} color={"#F58F00"} />
+
+                <Text
+                  numberOfLines={2}
+                  className="w-32 text-lg font-semibold text-black"
+                >
+                  TAAAAAAaaaaaaaaaaaaaaaaaaaaaa
+                </Text>
+              </View>
+
+              <AntDesign name="right" size={30} />
+            </Link> */}
+
+            <Link href={"/(menu)/personalData"} style={{ width: "100%" }}>
+              <CardMenu
+                color="bg-gray-700"
+                iconName="person-sharp"
+                iconSize={30}
+                title="Meus dados"
+                titleColor="text-black"
+                titleSize="text-xl"
+              />
             </Link>
           </>
         ) : (
