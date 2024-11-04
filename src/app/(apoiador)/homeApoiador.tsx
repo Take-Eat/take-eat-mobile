8b4a7d2a7ee4b29f56a2b0cdaf5a193e0ebb3dd8
isabelle9912 <isabellesaahirah28@gmail.com>
2024-11-04 12:42:23 -0300

    feat: header animado

--- src/app/(apoiador)/homeApoiador.tsx
+++ src/app/(apoiador)/homeApoiador.tsx
@@ -1,15 +1,35 @@
-import { StatusBar, Text, View, SafeAreaView, FlatList } from "react-native";
+import {
+  StatusBar,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  Animated,
+} from "react-native";
 import { globalStyles } from "@/src/assets/styles/Global";
+import { useRef } from "react";
 
-const data = [
-  { id: 1, title: "Item 1" },
-  { id: 2, title: "Item 2" },
-  { id: 3, title: "Item 3" },
-  { id: 4, title: "Item 4" },
-  { id: 5, title: "Item 5" },
-];
+const H_MAX_HEIGHT = 160;
+const H_MIN_HEIGHT = 1;
+const H_SCROLL_DISTANCE = H_MAX_HEIGHT - H_MIN_HEIGHT;
 
 export default function HomeApoiador() {
+  const data = [
+    { id: 1, title: "Item 1" },
+    { id: 2, title: "Item 2" },
+    { id: 3, title: "Item 3" },
+    { id: 4, title: "Item 4" },
+    { id: 5, title: "Item 5" },
+  ];
+
+  const scrollOffsetY = useRef(new Animated.Value(0)).current;
+
+  const headerScrollHeight = scrollOffsetY.interpolate({
+    inputRange: [0, H_SCROLL_DISTANCE],
+    outputRange: [H_MAX_HEIGHT, H_MIN_HEIGHT],
+    extrapolate: "clamp",
+  });
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar
@@ -18,20 +38,38 @@ export default function HomeApoiador() {
         translucent={false}
       />
 
-      <View className="w-full h-40 p-3 bg-primary items-center justify-center">
+      <Animated.View
+        style={{ height: headerScrollHeight }}
+        className="w-full p-3 bg-primary items-center justify-center absolute top-0 left-0 right-0 z-50 overflow-hidden"
+      >
         <Text style={globalStyles.heading1} className="color-white">
           Olá, Outback
         </Text>
-      </View>
+      </Animated.View>
 
       <FlatList
+        style={{ paddingTop: H_MAX_HEIGHT }}
         data={data}
         renderItem={({ item }) => (
-          <View>
+          <View className="h-80 p-5 border-b border-secondary-100">
             <Text>{item.title}</Text>
           </View>
         )}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollOffsetY } } }],
+          { useNativeDriver: false }
+        )}
+        scrollEventThrottle={16}
       />
     </SafeAreaView>
   );
 }
+
+/**
+ * w-full
+ * flex-row
+ * justify-between
+ * items-center
+ *
+ *
+ */
