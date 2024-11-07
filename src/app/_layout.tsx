68765218165c14731662ac61cbc0595f44c3f166
isabelle9912 <isabellesaahirah28@gmail.com>
2024-11-07 00:35:43 -0300

    fix: retirando index do rootLayout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -41,7 +41,6 @@ export default function RootLayout() {
 
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
-        <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack >
     </AuthProvider >
   );
