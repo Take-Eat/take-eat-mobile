27733b07e60f19d2241acd0159fea958a5e5ec84
isabelle9912 <isabellesaahirah28@gmail.com>
2024-11-07 00:30:13 -0300

    feat: página de login quando o usuario não está autenticado

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,20 +1,14 @@
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
-    return (
-        <Stack>
-            <Stack.Screen
-                name="sign-in"
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen
-                name="index"
-                options={{
-                    headerShown: false,
-                }}
-            />
-        </Stack>
-    );
-};
+  return (
+    <Stack>sss
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+}
