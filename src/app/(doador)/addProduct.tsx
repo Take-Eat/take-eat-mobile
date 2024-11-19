7618b0bc6178cb5377082e82483ee2e1f7762151
isabelle9912 <isabellesaahirah28@gmail.com>
2024-11-19 11:47:44 -0300

    feat: tela de adicionar produto com o zod

--- src/app/(doador)/addProduct.tsx
+++ src/app/(doador)/addProduct.tsx
@@ -1,178 +1,48 @@
 import React from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-  ScrollView,
-} from "react-native";
-import { Formik } from "formik";
-import * as Yup from "yup";
-import { colors, globalStyles } from "@/src/assets/styles/Global";
+import { ScrollView, Alert, Text } from "react-native";
+import { z } from "zod";
+import { FormCommon, FormSection } from "@/src/components";
+import { globalStyles } from "@/src/assets/styles/Global";
 
-const ProductSchema = Yup.object().shape({
-  name: Yup.string().required("Nome do produto é obrigatório"),
-  description: Yup.string().required("Descrição é obrigatória"),
-  price: Yup.number()
-    .required("Preço é obrigatório")
-    .positive("O preço deve ser maior que zero"),
-  category: Yup.string().required("Categoria é obrigatória"),
+const ProductSchema = z.object({
+  name: z.string({ required_error: "Nome do produto é obrigatório" }),
+  description: z.string({ required_error: "Descrição é obrigatória" }),
+  weight: z
+    .number({ required_error: "Peso é obrigatório" })
+    .positive("O peso deve ser maior que zero"),
+  category: z.string({ required_error: "Categoria é obrigatória" }),
 });
+
+const fields = [
+  { label: "Nome", key: "name" },
+  { label: "Descrição", key: "description", isTextArea: true }, // Marcar como área de texto
+  { label: "Peso", key: "weight" },
+  { label: "Categoria", key: "category" },
+];
 
 export default function AddProduct() {
   const handleSubmit = (values: any) => {
     console.log("Produto Adicionado:", values);
+    Alert.alert(
+      "Produto Adicionado!",
+      "Realmente deseja adicionar esse alimento?",
+      [{ text: "Confirmar", style: "default" }]
+    );
     // Dadinhos para api que não sabemos se vai dar tempo fazer '-'
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={[globalStyles.heading1, styles.title]}>
+    <ScrollView className="h-24 flex-grow bg-slate-50 p-5">
+      <Text className="text-center mb-5" style={globalStyles.heading1}>
         Adicionar Produto
       </Text>
 
-      <Formik
-        initialValues={{
-          name: "",
-          description: "",
-          price: "",
-          category: "",
-        }}
-        validationSchema={ProductSchema}
+      <FormSection
+        buttonText="Adicionar Alimento"
+        schema={ProductSchema}
+        fields={fields}
         onSubmit={handleSubmit}
-      >
-        {({
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          values,
-          errors,
-          touched,
-        }) => (
-          <View>
-            {/* Nome do Produto */}
-            <View style={styles.inputGroup}>
-              <Text style={styles.label}>Nome</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Digite o nome do produto"
-                onChangeText={handleChange("name")}
-                onBlur={handleBlur("name")}
-                value={values.name}
-              />
-              {touched.name && errors.name && (
-                <Text style={styles.error}>{errors.name}</Text>
-              )}
-            </View>
-
-            {/* Descrição */}
-            <View style={styles.inputGroup}>
-              <Text style={styles.label}>Descrição</Text>
-              <TextInput
-                style={[styles.input, styles.textArea]}
-                placeholder="Digite uma descrição"
-                onChangeText={handleChange("description")}
-                onBlur={handleBlur("description")}
-                value={values.description}
-                multiline
-              />
-              {touched.description && errors.description && (
-                <Text style={styles.error}>{errors.description}</Text>
-              )}
-            </View>
-
-            {/* Preço */}
-            <View style={styles.inputGroup}>
-              <Text style={styles.label}>Preço</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Digite o preço"
-                onChangeText={handleChange("price")}
-                onBlur={handleBlur("price")}
-                value={values.price}
-                keyboardType="numeric"
-              />
-              {touched.price && errors.price && (
-                <Text style={styles.error}>{errors.price}</Text>
-              )}
-            </View>
-
-            {/* Categoria */}
-            <View style={styles.inputGroup}>
-              <Text style={styles.label}>Categoria</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Digite a categoria"
-                onChangeText={handleChange("category")}
-                onBlur={handleBlur("category")}
-                value={values.category}
-              />
-              {touched.category && errors.category && (
-                <Text style={styles.error}>{errors.category}</Text>
-              )}
-            </View>
-
-            {/* Botão de Submissão */}
-            <TouchableOpacity
-              onPress={handleSubmit as any}
-              style={styles.button}
-            >
-              <Text style={styles.buttonText}>Adicionar Produto</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      </Formik>
+      />
     </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    backgroundColor: "#F7F7F7",
-    padding: 20,
-  },
-  title: {
-    textAlign: "center",
-    marginBottom: 20,
-  },
-  inputGroup: {
-    marginBottom: 15,
-  },
-  label: {
-    fontSize: 16,
-    color: "#333",
-    marginBottom: 5,
-  },
-  input: {
-    backgroundColor: "#FFFFFF",
-    borderRadius: 8,
-    paddingHorizontal: 10,
-    paddingVertical: 12,
-    fontSize: 14,
-    borderWidth: 1,
-    borderColor: "#DDD",
-  },
-  textArea: {
-    height: 100,
-    textAlignVertical: "top",
-  },
-  error: {
-    fontSize: 12,
-    color: "#FF0000",
-    marginTop: 5,
-  },
-  button: {
-    backgroundColor: colors.brand3,
-    padding: 15,
-    borderRadius: 8,
-    alignItems: "center",
-    marginTop: 20,
-  },
-  buttonText: {
-    color: "#FFF",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
