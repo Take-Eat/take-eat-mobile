c08844d88409613bc6866644c58eb49493d5d767
isabelle9912 <isabellesaahirah28@gmail.com>
2024-11-20 15:23:38 -0300

    feat: adicionando logout no menu

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -28,7 +28,7 @@ interface AuthContextType {
 export const AuthContext = createContext({} as AuthContextType);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  // SecureStore.setItemAsync("userType", "entregador");
+  // SecureStore.setItemAsync("userType", "guest");
 
   const [user, setUser] = useState<User | null>(null);
   const [userType, setUserType] = useState<UserType>("guest");
