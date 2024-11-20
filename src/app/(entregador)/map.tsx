9bd45c886ea039fb788b57e39187036222ea4d56
isabelle9912 <isabellesaahirah28@gmail.com>
2024-11-20 15:11:58 -0300

    feat: cancelar entrega

--- src/app/(entregador)/map.tsx
+++ src/app/(entregador)/map.tsx
@@ -1,7 +1,8 @@
+import { globalStyles } from "@/src/assets/styles/Global";
 import { MapContainer, MapSearchBar } from "@/src/components";
 import { calculatePrice } from "@/src/utils/calculatePrice";
 import { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 import { Region } from "react-native-maps";
 
 export default function AddressScreen() {
@@ -17,6 +18,23 @@ export default function AddressScreen() {
     setDistance(dist);
     setPrice(calculatePrice(dist));
     setDuration(dur);
+  };
+
+  const cancelDelivery = () => {
+    Alert.alert("Cancelar Entrega", "Deseja realmente cancelar esta entrega?", [
+      { text: "Cancelar", style: "cancel" },
+      {
+        text: "Sim",
+        style: "destructive",
+        onPress: () => {
+          setIsRunning(false);
+          setDestination(null);
+          setDistance(null);
+          setDuration(null);
+          setPrice(null);
+        },
+      },
+    ]);
   };
 
   return (
@@ -36,7 +54,7 @@ export default function AddressScreen() {
           onPress={() => setIsRunning(true)}
         >
           <Text className="text-white text-xl font-semibold">
-            Começar Corrida
+            Começar Entrega
           </Text>
         </TouchableOpacity>
       )}
@@ -50,9 +68,23 @@ export default function AddressScreen() {
             Tempo: {duration.toFixed(2)} min
           </Text>
 
-          <Text className="p-3 bg-primary rounded-lg text-white text-xl font-semibold top-3">
+          <Text className="p-3 mb-5 bg-primary rounded-lg text-white text-xl font-semibold top-3">
             Preço: R${price.toFixed(2)}
           </Text>
+          {/* Botão de Cancelar Entrega */}
+          {isRunning && (
+            <TouchableOpacity
+              className="px-3 py-4 items-center bg-alert"
+              style={[globalStyles.roundedRegular]}
+              onPress={() => {
+                cancelDelivery();
+              }}
+            >
+              <Text className="color-white" style={globalStyles.textLargerBold}>
+                Cancelar Entrega
+              </Text>
+            </TouchableOpacity>
+          )}
         </View>
       )}
     </View>
