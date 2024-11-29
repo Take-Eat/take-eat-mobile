2e0f0020a812c489df032b1ae757e0230e8c6762
peth <pethersonreis@gmail.com>
2024-11-29 18:03:39 -0300

    feat: landing page

--- src/app/(guest)/index.tsx
+++ src/app/(guest)/index.tsx
@@ -5,17 +5,26 @@ import { CustomButton } from "@/src/components";
 import ImgSVG1 from "@/src/assets/images/landing_page_img_1.svg"
 import ImgBackSVG1 from "@/src/assets/images/landing_page_back_1.svg"
 import ImgBackSVG2 from "@/src/assets/images/landing_page_back_2.svg"
+import ImgBackSVG3 from "@/src/assets/images/landing_page_back_3.svg"
+import ImgBackSVG4 from "@/src/assets/images/landing_page_back_4.svg"
+import ImgBackSVG5 from "@/src/assets/images/landing_page_back_5.svg"
 import Hr from "@/src/assets/images/line.svg"
 import ImgSVG2 from "@/src/assets/images/landing_page_img_2.svg"
 import ImgSVG3 from "@/src/assets/images/landing_page_img_3.svg"
 import ImgSVG4 from "@/src/assets/images/landing_page_img_4.svg"
 import ImgSVG5 from "@/src/assets/images/landing_page_img_5.svg"
 import ImgSVG6 from "@/src/assets/images/landing_page_img_6.svg"
+import ImgSVG7 from "@/src/assets/images/landing_page_img_7.svg"
+import ImgSVG8 from "@/src/assets/images/landing_page_img_8.svg"
+import ImgSVG9 from "@/src/assets/images/landing_page_img_9.svg"
+import ImgSVG10 from "@/src/assets/images/landing_page_img_10.svg"
+import ImgSVGEx from "@/src/assets/images/Ellipse 22.svg"
+import { ScreenFooter } from "react-native-screens";
 
 export default function GuestHomeScreen() {
   return (
     <ScrollView className="bg-white">
-      <View className="flex-1 p-4">
+      <View className="flex-1 p-4 gap-2">
         <View className="relative my-10 flex items-center gap-2">
           <View className="absolute left-40  bottom-56">
             <ImgBackSVG1 width={200} height={200} />
@@ -90,7 +99,7 @@ export default function GuestHomeScreen() {
             <View className="flex flex-row justify-between items-center">
               <View className="flex-1">
                 <Text style={[globalStyles.textRegular, { textAlign: "left" }]}>
-                  Para você empresa, você estará  contribuindo para a redução do desperdício, estará gerando impacto social e ambiental e também trará uma boa visibilidade para seu negócio
+                  Para você empresa, sua contribuição trará a redução do desperdício, estará gerando impacto social e ambiental, e também trará uma boa visibilidade para seu negócio
                 </Text>
                 <View className="bg-primary px-4 py-2 mt-4" style={[globalStyles.roundedRegular, { width: 100, marginRight: "auto", marginLeft: 0 }]}>
                   <Pressable onPress={() => { }}>
@@ -98,8 +107,8 @@ export default function GuestHomeScreen() {
                   </Pressable>
                 </View>
               </View>
-              <View >
-                <ImgSVG5 style={{ marginRight: -40 }} />
+              <View>
+                <ImgSVG7 width={100} height={150} style={{ marginRight: 0 }} />
               </View>
             </View>
             <Hr />
@@ -118,7 +127,101 @@ export default function GuestHomeScreen() {
             <ImgBackSVG2 />
           </View>
         </View>
+        <View className="mt-10 px-2 flex gap-2 items-center relative">
+          <Text className="text-center text-primary" style={globalStyles.heading2}>Impacto Social</Text>
+          <View className="flex flex-row justify-between items-center w-full">
+            <View className="flex-1 justify-center max-w-[250px]">
+              <Text style={[globalStyles.textRegular, { textAlign: "left" }]}>
+                Nosso App conta com diversos colaboradores, junto a eles conseguimos evitar o desperdício uma grande quantidade de alimentos              </Text>
+              {/* <View className="bg-primary px-4 py-2 mt-4" style={[globalStyles.roundedRegular, { width: 100, marginRight: "auto", marginLeft: 0 }]}>
+                <Pressable onPress={() => { }}>
+                  <Text className="text-white text-center">Saiba mais</Text>
+                </Pressable>
+              </View> */}
+            </View>
+            <View className="absolute right-[-35]">
+              <ImgSVG8 width={180} height={150} style={{ zIndex: -1 }} />
+            </View>
+          </View>
+          <Hr />
+        </View>
+        <View className="mt-14 mb-10 px-2 flex flex-row flex-wrap justify-center items-center gap-4">
+          <View className="flex justify-center">
+            <Text style={globalStyles.textRegular} className="text-center">Empresas Colaboradoras</Text>
+            <Text style={globalStyles.heading3} className="text-center">532</Text>
+          </View>
+          <View className="flex justify-center">
+            <Text style={globalStyles.textRegular} className="text-center">ONG's e Instituições</Text>
+            <Text style={globalStyles.heading3} className="text-center">100</Text>
+          </View>
+          <View className="flex justify-center">
+            <Text style={globalStyles.textRegular} className="text-center">Alimentos Aproveitados</Text>
+            <Text style={globalStyles.heading3} className="text-center">2.5 toneladas</Text>
+          </View>
+        </View>
+        <View className="mt-10 px-2 flex gap-2 justify-center items-center relative">
+          <View className="flex justify-between items-center w-full gap-4">
+            <View className="flex items-center max-w-[250px]">
+              <Text style={[globalStyles.heading2, { textAlign: "center" }]}>
+                O que você está esperando? Venha agora fazer parte da nossa iniciativa
+              </Text>
+              <View className="bg-primary px-4 py-2 mt-4" style={[globalStyles.roundedRegular, { width: 100 }]}>
+                <Pressable onPress={() => { }}>
+                  <Text className="text-white text-center">Saiba mais</Text>
+                </Pressable>
+              </View>
+            </View>
+            <Hr />
+            <View>
+              <ImgSVG9 width={180} height={150} />
+            </View>
+          </View>
+          <View className="absolute">
+            <ImgBackSVG3 width={380} />
+          </View>
+        </View>
+        <View className="mt-10 px-2 flex gap-2 justify-center items-center relative">
+          <Text className="text-center text-primary" style={globalStyles.heading2}>Parceiros</Text>
+          <View className="flex justify-between items-center w-full gap-4">
+            <View className=" flex flex-row gap-2 mt-4">
+              <ImgSVGEx />
+              <ImgSVGEx />
+              <ImgSVGEx />
+              <ImgSVGEx />
+              <ImgSVGEx />
+            </View>
+            <View>
+              <ImgSVG10 />
+            </View>
+          </View>
+          <View className="absolute">
+            <ImgBackSVG4 width={380} style={{ zIndex: -1 }} />
+          </View>
+        </View>
+        <View className="my-10 px-2 flex gap-2 justify-center items-center relative">
+          <Text className="text-center text-primary" style={globalStyles.heading2}>Nosso Time</Text>
+          <View className="flex justify-between items-center w-full gap-4">
+            <View className=" flex flex-row gap-4 mt-4">
+              <View className="flex items-center">
+                <ImgSVGEx />
+                <Text style={globalStyles.textRegular}>Nome da Lenda</Text>
+              </View>
+              <View className="flex items-center">
+                <ImgSVGEx />
+                <Text style={globalStyles.textRegular}>Nome da Lenda</Text>
+              </View>
+              <View className="flex items-center">
+                <ImgSVGEx />
+                <Text style={globalStyles.textRegular}>Nome da Lenda</Text>
+              </View>
+            </View>
+          </View>
+          <View className="absolute right-8 top-3">
+            <ImgBackSVG5 width={380} style={{ zIndex: -1 }} />
+          </View>
+        </View>
       </View>
+      <View className="mt-16 p-5 bg-primary"><Text className="text-center text-white" style={globalStyles.heading3}>₢Copyright - Todos direitos reservados - Take Eat</Text></View>
     </ScrollView >
   );
 }
