726af4e5446776df7f6db872c9fbde96fc616e0e
isabelle9912 <isabellesaahirah28@gmail.com>
2024-12-03 13:44:02 -0300

    feat: tela de intro e icone

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,11 +4,13 @@ import { colors } from "@/src/assets/styles/Global";
 import { router } from "expo-router";
 import LogoSvg from "@/src/assets/images/logo.svg";
 
+
 export default function Header() {
   return (
     <View className="w-full h-24 flex-row justify-between items-center">
       <Pressable className="h-full flex justify-center">
-        <LogoSvg width={140} height={100} />
+        {/* <LogoSvg width={140} height={140} /> */}
+        <Image source={require("@/src/assets/images/logo_take_eat_black.png")} />
       </Pressable>
 
       <TouchableOpacity onPress={() => router.push("/(menu)/notifications")}>
